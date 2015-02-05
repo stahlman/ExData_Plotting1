57a8e642d53b59bd06e1d3af2bba0e95c09229dc
stahlman <jonstahlman@gmail.com>
2015-02-05 14:58:14 -0500

    adding processing step

--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@ curl https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consu
 
 tar -xvf  exdata_data_household_power_consumption.zip
 
-grep '^1/2/2007\|^2/2/2007' household_power_consumption.txt > household_power_consumption_reduced.txt
+head -1 household_power_consumption.txt >  household_power_consumption_reduced.txt
+
+grep '^1/2/2007\|^2/2/2007' household_power_consumption.txt >> household_power_consumption_reduced.txt
